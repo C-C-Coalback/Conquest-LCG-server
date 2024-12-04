5aca3ce95a0326ef9a38030ac36f7ac07757b980
C-C-Coalback <argarfield18@gmail.com>
2024-12-04 15:53:32 +0100

    Shameless stealing again, multithreading this time. Should work I hope lol.

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
 import socket
+from threading import *
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8089))
-server_socket.listen(20) # become a server socket, maximum 5 connections
 
+class Client(Thread):
+    def __init__(self, class_socket, class_address):
+        Thread.__init__(self)
+        self.sock = class_socket
+        self.addr = class_address
+        self.stored_message = ""
+        self.start()
+
+    def run(self):
+        while 1:
+            message = self.sock.recv(1024).decode()
+            print('Client sent:', message)
+            self.sock.send(bytes("Confirm received", 'UTF-8'))
+            self.stored_message = message.split(sep="#")
+            print("Stored message:", self.stored_message)
+
+server_socket.listen(20)
+print ('server started and listening')
 while True:
     connection, address = server_socket.accept()
-    buf = connection.recv(64)
-    string = buf.decode("utf-8")
-    string = string.split(sep="#")
-    if len(string) > 0:
-        print(string)
-        for i in range(len(string)):
-            print(string[i])
-        connection.send(bytes("Confirm received", 'UTF-8'))
-        break
+    Client(connection, address)
