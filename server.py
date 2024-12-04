06a1839f4f698917e7db6e30324c5e66d9bd2d55
C-C-Coalback <argarfield18@gmail.com>
2024-12-04 15:25:18 +0100

    Incredibly small change to split text

--- server.py
+++ server.py
@@ -2,12 +2,13 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8089))
-server_socket.listen(5) # become a server socket, maximum 5 connections
+server_socket.listen(20) # become a server socket, maximum 5 connections
 
 while True:
     connection, address = server_socket.accept()
     buf = connection.recv(64)
     string = buf.decode("utf-8")
+    string = string.split(sep="#")
     if len(string) > 0:
         print(string)
         for i in range(len(string)):
