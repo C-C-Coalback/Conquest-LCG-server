91097406c6a6f6aad5d611b4bd03e2db6b143eaa
C-C-Coalback <argarfield18@gmail.com>
2024-12-12 13:35:26 +0100

    Program moves into the Game class, can send and receive info.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 from threading import *
-import pygame
+# import pygame
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8089))
@@ -12,18 +12,6 @@ class Game(Thread):
         self.client_one_sock = client_one.sock
         self.client_one_addr = client_one.addr
         self.stored_message_p_one = client_one.stored_message
-        self.running = True
-        self.c = Condition()
-
-    def print_stored_1(self):
-        print(self.stored_message_p_one)
-
-class Client(Thread):
-    def __init__(self, class_socket, class_address):
-        Thread.__init__(self)
-        self.sock = class_socket
-        self.addr = class_address
-        self.stored_message = ""
         self.current_board_state = ""
         self.running = True
         self.c = Condition()
@@ -33,12 +21,29 @@ class Client(Thread):
         Thread(target=self.recv).start()
         Thread(target=self.send_info).start()
 
-    def get_stored_message(self):
-        self.c.acquire()
-        self.c.notify_all()
-        message = self.stored_message
-        self.c.release()
-        return message
+    def print_stored_1(self):
+        print(self.stored_message_p_one)
+
+    def print_current_board_state(self):
+        print(self.current_board_state)
+
+    def recv(self):
+        try:
+            while self.running:
+                message = self.client_one_sock.recv(1024).decode()
+                if not message:
+                    break
+                print('Client sent:', message)
+                self.c.acquire()
+                self.c.notify_all()
+                self.stored_message_p_one = message.split(sep="#")
+                print("Stored message:", self.stored_message_p_one)
+                self.c.release()
+                if message == "QUIT":
+                    self.running = False
+                    self.client_one_sock.close()
+        except ConnectionResetError:
+            print("Existing connection closed by host")
 
     def send_info(self):
         try:
@@ -59,9 +64,30 @@ class Client(Thread):
                                "#Chaos Fanatics(H!E!0)/Possessed(H!R!2)/Chaos Fanatics(H!R!0)#NONE#NONE#Shoota Mob(H!R!0)#NONE#NONE#Goff Boyz(H!R!1)"
                                "#Alpha Legion Infiltrator(H!R!0)/Goff Boyz(H!R!0)/Goff Boyz(H!R!1)#Goff Boyz(H!E!1)#NONE"
                                "#NONE#NONE#Rogue Trader(H!R!0)#NONE")
-                self.sock.send(bytes(message, 'UTF-8'))
+                self.client_one_sock.send(bytes(message, 'UTF-8'))
         except OSError:
             print("Socket closed")
+
+class Client(Thread):
+    def __init__(self, class_socket, class_address):
+        Thread.__init__(self)
+        self.sock = class_socket
+        self.addr = class_address
+        self.stored_message = ""
+        self.running = True
+        self.c = Condition()
+        self.start()
+
+    def run(self):
+        Thread(target=self.recv).start()
+        # Thread(target=self.send_info).start()
+
+    def get_stored_message(self):
+        self.c.acquire()
+        self.c.notify_all()
+        message = self.stored_message
+        self.c.release()
+        return message
 
     def recv(self):
         try:
@@ -78,6 +104,9 @@ class Client(Thread):
                 if message == "QUIT":
                     self.running = False
                     self.sock.close()
+                if message == "BEGIN GAME":
+                    self.running = False
+                    Game(self)
         except ConnectionResetError:
             print("Existing connection closed by host")
 
@@ -88,9 +117,6 @@ while True:
     new_client = Client(connection, address)
     client_array.append(new_client)
     client_array[0].stored_message = "test"
-    game_object = Game(client_array[0])
-    print(game_object.print_stored_1())
-    _ = pygame.time.wait(1000)
     keep_server = "n"
     if keep_server == "n":
         break
